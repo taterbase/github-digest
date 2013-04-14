b8c76c09426fdac2f9b206b3f67b65dcc51f9bd6
taterbase <shankga@gmail.com>
2013-04-13 20:03:39 -0600

    Added session support

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express')
   , app     = express()
+  , config  = require('./config')
   ;
+
+app.use(express.bodyParser());
+app.use(express.cookieParser());
+app.use(express.cookieSession({secret: config.sid}));
 
 app.use(require('./lib/main'));
 app.use(require('./lib/auth'));
